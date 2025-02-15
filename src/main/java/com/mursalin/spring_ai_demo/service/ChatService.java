fb8313df14286b8358721b335bb7f205fd8817ed
mohammad-mursalin <mursalinlamon@gmail.com>
2025-02-15 11:33:30 +0600

    environment variables and web client

--- src/main/java/com/mursalin/spring_ai_demo/service/ChatService.java
+++ src/main/java/com/mursalin/spring_ai_demo/service/ChatService.java
@@ -4,6 +4,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
+
+    @Value("${gemini.api.key}")
+    private String gemini_api_key;
+
+    @Value("${gemini.api.url}")
+    private String gemini_api_url;
+
+    private final WebClient webClient;
+
+    public ChatService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
     public String generateReply(String message) {
 
     }
