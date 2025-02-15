f3a80fd9e266736bc5380643ac5cd64681a0fe7c
mohammad-mursalin <mursalinlamon@gmail.com>
2025-02-15 12:36:54 +0600

    lombok used

--- src/main/java/com/mursalin/spring_ai_demo/controller/ChatController.java
+++ src/main/java/com/mursalin/spring_ai_demo/controller/ChatController.java
@@ -1,20 +1,21 @@
 package com.mursalin.spring_ai_demo.controller;
 
 import com.mursalin.spring_ai_demo.service.ChatService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/ai-chat")
+@AllArgsConstructor
 public class ChatController {
 
     private final ChatService chatService;
 
-    public ChatController(ChatService chatService) {
-        this.chatService = chatService;
-    }
+//    public ChatController(ChatService chatService) {
+//        this.chatService = chatService;
+//    }
 
+    @PostMapping
     public String chatReply(@RequestBody String message) {
         return chatService.generateReply(message);
     }
